e859de3e0e4f33c1c8888997b7b3a83109e602ce
Shekhar Gulati <shekhargulati84@gmail.com>
2013-06-05 18:58:26 +0530

    LocalJobs application with Google Maps and Direction API

---
+++ src/main/java/com/localjobs/mongo/Job.java
@@ -0,0 +1,117 @@
+package com.localjobs.mongo;
+
+import java.util.Arrays;
+
+import org.bson.types.ObjectId;
+
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+
+public class Job {
+
+	private String id;
+	private String companyName;
+
+	private String jobTitle;
+
+	private double distance;
+
+	private String[] skills;
+
+	private String formattedAddress;
+
+	private Object longitude;
+
+	private Object latitude;
+
+	public Job() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Job(BasicDBObject result) {
+		
+		this.distance = result.getDouble("dis");
+		BasicDBObject jobObj = (BasicDBObject) result.get("obj");
+		Object objectId = jobObj.get("_id");
+		this.id = ((ObjectId)objectId).toString();
+		this.companyName = ((BasicDBObject) jobObj.get("company"))
+									.getString("companyName");
+		this.jobTitle = jobObj.getString("jobTitle");
+		BasicDBList locationList = (BasicDBList)jobObj.get("location");
+		this.longitude = locationList.get(0);
+		this.latitude = locationList.get(1);
+		this.formattedAddress = jobObj.getString("formattedAddress");
+		this.skills = ((BasicDBList) jobObj.get("skills")).toArray(new String[0]);
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getJobTitle() {
+		return jobTitle;
+	}
+
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+
+	public double getDistance() {
+		return distance;
+	}
+
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+
+	public String[] getSkills() {
+		return skills;
+	}
+
+	public void setSkills(String[] skills) {
+		this.skills = skills;
+	}
+
+	public String getFormattedAddress() {
+		return formattedAddress;
+	}
+
+	public void setFormattedAddress(String formattedAddress) {
+		this.formattedAddress = formattedAddress;
+	}
+	public void setLongitude(Object longitude) {
+		this.longitude = longitude;
+	}
+	
+	public Object getLongitude() {
+		return longitude;
+	}
+	
+	public void setLatitude(Object latitude) {
+		this.latitude = latitude;
+	}
+	public Object getLatitude() {
+		return latitude;
+	}
+
+	
+	public String getId() {
+		return id;
+	}
+	
+	@Override
+	public String toString() {
+		return "Job [companyName=" + companyName + ", jobTitle=" + jobTitle
+				+ ", distance=" + distance + ", skills="
+				+ Arrays.toString(skills) + ", formattedAddress="
+				+ formattedAddress + "]";
+	}
+	
+	
+	
+	
+}
